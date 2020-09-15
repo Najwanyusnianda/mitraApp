7d528c87b1b7df66e200db10e0e2479929cf8e10
najwanyusnianda <najwanyusnianda@gmail.com>
2020-09-15 21:09:44 +0700

    init

---
+++ resources/views/master.blade.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+   
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>MitraApp</title>
+   <!-- <link rel = "icon" href = "https://media.geeksforgeeks.org/wp-content/cdn-uploads/gfg_200X200.png" type = "image/x-icon"> -->
+    <!-- Favicon -->
+  
+   <!-- <link href="{{asset('vendor_temp/argon/assets/img/brand/favicon.png')}}" rel="icon" type="image/png">-->
+    <!-- Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700" rel="stylesheet">
+   <!-- <link href="https://fonts.googleapis.com/css2?family=Montserrat&display=swap" rel="stylesheet">-->
+
+    <!-- Icons -->
+    
+    <link href="{{asset('vendor_temp/argon/assets/vendor/nucleo/css/nucleo.css')}}" rel="stylesheet">
+    <link href="{{asset('vendor_temp/argon/assets/vendor/@fortawesome/fontawesome-free/css/all.min.css')}}" rel="stylesheet">
+    <!-- Argon CSS -->
+    <link href="{{asset('vendor_temp/argon/assets/css/argon.min.css')}}" rel="stylesheet">
+    <style>
+ 
+    </style>
+</head>
+
+<body>
+    @include('master_navbar')
+    @include('master_sidebar')
+    <!-- Core -->
+    <script src="{{asset('vendor_temp/argon/assets/vendor/jquery/dist/jquery.min.js')}}"></script>
+    <script src="{{asset('vendor_temp/argon/assets/vendor/bootstrap/dist/js/bootstrap.bundle.min.js')}}"></script>
+
+    <!-- Argon JS -->
+    <script src="{{asset('vendor_temp/argon/assets/js/argon.min.js')}}"></script>
+</body>
+
+</html>
